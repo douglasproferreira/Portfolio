c64ca3a06c722d9061027de7fe08aa8bdfbee116
douglasproferreira <douglas.ferreira2@estudante.ifms.edu.br>
2019-03-25 07:24:19 -0400

    Processando o erro

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', function(req, res){
+    res.sendfile('./src/view/index.html');
+  });
+  
+  var porta = process.env.PORT || 8080;
+  app.listen(porta);
