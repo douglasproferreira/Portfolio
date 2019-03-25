c4c9a2ed3d47dbac52cb8776e4e5bc8e932d7fb1
douglasproferreira <douglas.ferreira2@estudante.ifms.edu.br>
2019-03-25 07:30:18 -0400

    Arrumando

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', function(req, res){
-    res.sendfile('./src/view/index.html');
+    res.sendfile('./index.php');
   });
   
   var porta = process.env.PORT || 8080;
