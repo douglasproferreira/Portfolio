969c7e8a3421f04f305046fc68b4b3b0f14308e1
douglasproferreira <douglas.ferreira2@estudante.ifms.edu.br>
2019-03-26 07:51:50 -0400

    Arrumando img

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
 const app = express();
+
+// make express look in the public directory for assets (css/js/img)
+
+
 app.use(express.json());
+
+app.use(express.static(__dirname + '/css'));
 
 app.get('/', function(req, res){
     res.sendfile('./index.html');
