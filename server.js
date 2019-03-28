464ff34689a2c25d07b0d2dfe581960a6fffc431
douglasproferreira <douglas.ferreira2@estudante.ifms.edu.br>
2019-03-28 11:19:21 -0400

    Arrumando Nodejs

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require('express');
 
 const app = express();
 
-// make express look in the public directory for assets (css/js/img)
-
-
 app.use(express.json());
 
-app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/public'));
+
+//console.log(__dirname)
 
 app.get('/', function(req, res){
     res.sendfile('./index.html');
   });
   
-  var porta = process.env.PORT || 8080;
-  app.listen(porta);
+var porta = process.env.PORT || 8080;
+app.listen(porta);
